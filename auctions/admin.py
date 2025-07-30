90dd5f331c3334c9b0d8e24d8d720e975f68cbf1
Robin <rovan.rangi@gmail.com>
2025-07-30 21:32:43 +0530

    added admin

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Auctions, Bids, Comments, User
+
 # Register your models here.
+
+admin.site.register(Auctions)
+admin.site.register(Bids)
+admin.site.register(Comments)
+admin.site.register(User)
