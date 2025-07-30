f40dd5f211854023dd7ddf85ba9b9451af605d05
Robin <rovan.rangi@gmail.com>
2025-07-30 10:18:53 +0530

    initial commit

---
+++ auctions/models.py
@@ -0,0 +1,79 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    watchlist = models.ManyToManyField(
+        "auctions.Auctions",
+        blank=True,
+        related_name="watchlist_users",
+    )  # ManyToManyField to Auctions model for watchlist functionality
+
+
+class Auctions(models.Model):
+    auction_id = models.BigAutoField(primary_key=True)
+    auction_name = models.CharField(max_length=1000)
+    # item_image = models.ImageField(upload_to='item_images/')
+    item_image = models.CharField(
+        max_length=10000, blank=True, null=True, default=None
+    )
+    item_category = models.CharField(max_length=1000, blank=True, null=True, default=None)
+    item_name = models.CharField(max_length=1000, default="No Name")
+    item_description = models.CharField(max_length=10000, default="None")
+    starting_bid = models.FloatField()
+    listing_creation_time = models.DateTimeField(auto_now_add=True)
+    listed_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="listed_items"
+    )
+    current_bid = models.FloatField()
+    current_bid_by = models.ForeignKey(
+        User, blank=True, null=True,
+        default=None,
+        on_delete=models.SET_NULL,
+        related_name="winning_bids"
+    )
+    status = models.CharField(max_length=4, default="Sale")
+    sold_by = models.ForeignKey(
+        User, blank=False,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name="sold_items",
+        default=None,
+    )
+    sold_to = models.ForeignKey(
+        User, blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name="purchased_items",
+    )
+
+    def __str__(self):
+        if self.status != "Sold":
+            return f"{self.auction_id}: {self.auction_name} selling {self.item_name} at the bid of ${self.current_bid}"
+
+        return f"{self.auction_id}: {self.auction_name} sold {self.item_name} at the bid of ${self.current_bid} to {self.sold_to}"
+
+
+class Bids(models.Model):
+    bid_id = models.BigAutoField(primary_key=True)
+    auction_id = models.ForeignKey(
+        Auctions, on_delete=models.CASCADE, related_name="auction_bids"
+    )
+    bid_amount = models.FloatField()
+
+    def __str__(self):
+        return f"{self.bid_id}: ${self.bid_amount} for {self.auction_id}"
+
+
+class Comments(models.Model):
+    commment_id = models.BigAutoField(primary_key=True)
+    auction_id = models.ForeignKey(
+        Auctions, on_delete=models.CASCADE, related_name="auction_comments"
+    )
+    comment_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="user_comments"
+    )
+    comment_content = models.CharField(max_length=10000)
+
+    def __str__(self):
+        return f"{self.commment_id}: {self.comment_by} commented on {self.auction_id} that \"{self.comment_content}\""
---
+++ auctions/views.py
@@ -0,0 +1,160 @@
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
+from .models import User, Auctions, Bids, Comments
+
+
+def index(request):
+    return render(request, "auctions/index.html", {
+        "auctions": Auctions.objects.all(),
+    })
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("auctions:index"))
+        else:
+            return render(request, "auctions/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "auctions/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("auctions:index"))
+
+
+@login_required
+def new_listing(request):
+    if request.method == "POST":
+        auction_name = request.POST["auction_name"]
+        item_name = request.POST["item_name"]
+        item_description = request.POST["item_description"]
+        starting_bid = request.POST["starting_bid"]
+        item_image = request.POST["item_image"]
+        status = request.POST["status"]
+        item_category = request.POST["item_category"]
+        if status != "Sale":
+            return render(request, "auctions/new_listing.html", {
+                "message": f"The status cam't be {status} while creating a new listing."
+            })
+        # Convert starting_bid to float
+        try:
+            starting_bid = float(starting_bid)
+        except ValueError:
+            return render(request, "auctions/new_listing.html", {
+                "message": "Starting bid must be a number."
+            })
+
+        try:
+            auction = Auctions(
+                auction_name=auction_name,
+                item_name=item_name,
+                item_description=item_description,
+                starting_bid=starting_bid,
+                item_image=item_image,
+                listed_by=request.user,
+                current_bid=starting_bid,
+                status=status,
+                sold_to=None,
+                item_category=item_category
+            )
+            auction.save()
+        except Exception as e:
+            return render(request, "auctions/new_listing.html", {
+                "message": f"Something wrong happened: {e}"
+            })
+        return render(request, "auctions/new_listing.html", {
+            "message": f"Lisiting for {auction_name} created successfully!"
+        })
+
+    return render(request, "auctions/new_listing.html")
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "auctions/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "auctions/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("auctions:index"))
+    else:
+        return render(request, "auctions/register.html")
+
+
+def listing_page(request, auction_id):
+
+    auction = Auctions.objects.get(pk=auction_id)
+    if request.method == "POST" and request.user:
+
+        try:
+            bid = float(request.POST["bid"])
+        except Exception as e:
+            return render(request, "auction/listing_page.html", {
+                "message_bid": f"Bid needs to be a number: {e}"
+            })
+
+        if bid > auction.current_bid:
+            auction.current_bid = bid
+            auction.current_bid_by = request.user
+            return render(request, "auction/listing_page.html", {
+                "message_bid": f"Bid of ${auction.current_bid} placed successfully!"
+            })
+
+        elif (bid == auction.current_bid and
+              auction.starting_bid == auction.current_bid and
+              auction.current_bid_by is None):
+            auction.current_bid_by = request.user
+            return render(request, "auction/listing_page.html", {
+                "message_bid": f"Bid of ${auction.current_bid} placed successfully!"
+            })
+
+        else:
+            return render(request, "auctions/listing_page.html", {
+                "message_bid": f"Ther starting bid has already been placed; Your bid must be higher than {auction.current_bid}"
+            })
+
+    elif request.method == "POST" and not request.user:
+        return render(request, "auctions/listing_page.html", {
+            "message_bid": "The user must be loged in to interact with the listings."
+        })
+
+    return render(request, "auctions/listing_page.html", {
+        "a": auction
+    })
+
+
+def watchlist(request, auction_id):
+    if request.method == "POST":
+        pass
